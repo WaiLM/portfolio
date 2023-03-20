8622060fd2bf5a0ad1e4c251a1c1c3c68b6eb8d9
WaiLM <99259751+WaiLM@users.noreply.github.com>
2023-03-20 12:29:02 +1100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('#burger').on('click', function() {
+  $('#modal-menu').toggleClass('hidden')
+})
+
+$('#close-btn').on('click', function() {
+  $('#modal-menu').toggleClass('hidden')
+})
+
+$('#close-modal').on('click', function() {
+  $('#modal-menu').toggleClass('hidden')
+})
+
+$('#close-modal-2').on('click', function() {
+  $('#modal-menu').toggleClass('hidden')
+})
+
+$('#close-modal-3').on('click', function() {
+  $('#modal-menu').toggleClass('hidden')
+})
